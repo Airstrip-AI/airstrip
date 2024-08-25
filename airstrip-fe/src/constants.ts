8b0dcabacf30a57f45670e08749fcba67d81d97f
yongwee <yongwee1@gmail.com>
2024-08-25 20:25:35 +0800

    Update landing page github link

--- airstrip-fe/src/constants.ts
+++ airstrip-fe/src/constants.ts
@@ -1,7 +1,7 @@
 import { UserRole } from '@/utils/backend/client/common/types';
 
 export const PRODUCT_NAME = 'Airstrip';
-export const GITHUB_REPO_URL = 'https://github.com/hendychua/airstrip';
+export const GITHUB_REPO_URL = 'https://github.com/Airstrip-AI/airstrip';
 
 export const BE_API_HOST = process.env.NEXT_PUBLIC_BE_API_HOST;
 
