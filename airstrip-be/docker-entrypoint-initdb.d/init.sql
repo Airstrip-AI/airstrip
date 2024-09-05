941e1875a6a65d4f3aba5e0b8ef30165aa27da34
Hendy <hendychua@gmail.com>
2024-09-05 10:18:38 +0800

    Grant create schema privileges to user.

--- airstrip-be/docker-entrypoint-initdb.d/init.sql
+++ airstrip-be/docker-entrypoint-initdb.d/init.sql
@@ -1,5 +1,6 @@
 CREATE ROLE airstrip LOGIN PASSWORD 'airstrip';
 CREATE DATABASE airstrip;
+GRANT CREATE ON DATABASE airstrip TO airstrip;
 \c airstrip;
 CREATE SCHEMA airstrip AUTHORIZATION airstrip;
 CREATE EXTENSION "uuid-ossp";
