55a8d8abfc1eff71ac4e544ef38fc09e7d0e76c8
yongwee <yongwee1@gmail.com>
2024-09-12 15:17:44 +0800

    Booleanify value in optional-features action

--- airstrip-fe/src/actions/optional-features/index.ts
+++ airstrip-fe/src/actions/optional-features/index.ts
@@ -10,6 +10,7 @@ import { unstable_cache as cache } from 'next/cache';
 export const checkOptionalFeatures = cache(async () => {
   return {
     memoryAllowed: !!MEM0_API_KEY,
-    knowledgeBaseAllowed: !!OPENAI_EMBEDDING_API_KEY && BLOB_STORAGE_ACCESS_KEY,
+    knowledgeBaseAllowed:
+      !!OPENAI_EMBEDDING_API_KEY && !!BLOB_STORAGE_ACCESS_KEY,
   };
 });
